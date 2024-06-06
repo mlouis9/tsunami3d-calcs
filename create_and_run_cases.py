b414b3b69af3299dcb1a6763b9baca716b9cb09a
Matthew Louis <mlouis9@gatech.edu>
2024-06-06 14:13:18 -0400

    Updated to allow calling using an MPI script to parallelize runs to the extent possible on Sawtooth

--- create_and_run_cases.py
+++ create_and_run_cases.py
@@ -1,4 +1,17 @@
 from direct_pert_calcs import Cases
+import sys
 
-cases = Cases('case_parameters.yml', 'tsunami-3d-k6')
-cases.run_cases('sphere_template.inp')
+if __name__ == '__main__':
+    # First parse the cases
+    cases = Cases('case_parameters.yml', 'tsunami-3d-k6').cases
+
+    if len(sys.argv) != 1:
+        # If a specific case is requested, run that (useful for doing multiple runs in parallel)
+        case = cases[int(sys.argv[1])]
+        case.create_input_file('sphere_template.inp')
+        case.run_case()
+    else:
+        # RUn all inputs
+        for case in cases:
+            case.create_input_file('sphere_template.inp')
+            case.run_case()
